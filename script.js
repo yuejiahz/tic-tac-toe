0909fa2f3f640baac4fac41bd054e50098a0c119
yujiah23 <yuejialow@gmail.com>
2021-05-19 16:46:17 +0800

    display result on gameboard and improved layout

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const ticTacToe=(function (h){
+    let result=['X','O','X','O','X','O','X','O','O'];
+    const spaces=document.querySelectorAll(".space");
+    spaces.forEach((space)=> {
+        space.textContent=result[space.id[6]-1]; 
+    });
+ 
+})();
+
 
