ffdbc603e7fbf307bc0ef8053148a125a3343f6f
yujiah23 <yuejialow@gmail.com>
2021-05-26 01:34:13 +0800

    detect fair game

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
-const gameBoard =((player)=>{
+const gameBoard =(()=>{
 
 let gameBoardArr=[[],[],[]];
 let player1Marking=[];
 let player2Marking=[];
 
 let winner = null;
-let turns = 0;
 
 const playerFactory = (name, markingChoice, turn)=>{
 return { name, markingChoice, turn }
@@ -15,11 +14,19 @@ let player1=playerFactory('player 1','X',true);
 let player2=playerFactory('player 2','O',false);
 
 const displayController=(()=>{
-const spaces=document.querySelectorAll(".space");
-spaces.forEach((space)=> space.textContent=gameBoardArr[space.id[0]][space.id[1]]);
+    let checkedSpace=0;
+    const spaces=document.querySelectorAll(".space");
+    spaces.forEach((space)=> space.textContent=gameBoardArr[space.id[0]][space.id[1]]);
+    spaces.forEach((space)=> {
+        if(space.textContent){
+            checkedSpace++;
+        }
+        if(checkedSpace == 9){
+        winner="It's a fair game";
+        announceWinner(winner);
+        }
+        });
 });
-
-
 
 function checkWinner(playerArr) {
     let count=[0,0,0,0,0,0,0,0];
@@ -39,7 +46,7 @@ function checkWinner(playerArr) {
             if(winningArr[i].includes(playerArr[l])){
                 count[i]+=1;
             }
-            if(count[i]==3){
+            if(count[i] == 3){
                 return  true; 
             }
         }
@@ -51,19 +58,33 @@ const playerTurns=((event)=>{
     if (player1.turn == true && event.target.textContent ==''){     
         gameBoardArr[event.target.id[0]][event.target.id[1]]=player1.markingChoice;
         player1Marking.push(Number(event.target.id));
-        player1.turn=false;
-        player2.turn=true;
-        checkWinner(player1Marking);
+        player1.turn = false;
+        player2.turn = true;
+        const status = checkWinner(player1Marking);
+        if(status == true){
+             winner = 'player 1';
+             player2.turn = false;
+        }
     } else if (player2.turn == true && event.target.textContent ==''){
         gameBoardArr[event.target.id[0]][event.target.id[1]]=player2.markingChoice;
         player2Marking.push(Number(event.target.id));
-        player1.turn=true;
-        player2.turn=false;
-        checkWinner(player2Marking);
+        player1.turn = true;
+        player2.turn = false;
+        const status = checkWinner(player2Marking);
+        if(status == true){
+            player1.turn =false;
+            winner = 'player 2';
+       }
     }
     displayController();
-   
+    if(winner){
+        announceWinner(winner);
+    }
 });
+
+function announceWinner(winner){
+   console.log(winner);    
+}
 
 window.addEventListener('click', playerTurns);
 
